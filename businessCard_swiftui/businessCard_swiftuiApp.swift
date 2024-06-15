963602105ac4f096f8b9acd5777597a5701179c5
zakashour <zak.ashour@trucesoftware.com>
2024-06-15 06:53:03 -0500

    Initial Commit

---
+++ businessCard_swiftui/businessCard_swiftuiApp.swift
@@ -0,0 +1,17 @@
+//
+//  businessCard_swiftuiApp.swift
+//  businessCard_swiftui
+//
+//  Created by Zak Ashour on 6/15/24.
+//
+
+import SwiftUI
+
+@main
+struct businessCard_swiftuiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
